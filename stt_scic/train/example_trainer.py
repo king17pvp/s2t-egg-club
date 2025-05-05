48fd07164784feb4ae9b8ad457d5747c9a0d2e9a
King17pvp <tuanhai1304@gmail.com>
2025-05-05 23:35:48 +0700

    Init

---
+++ stt_scic/train/example_trainer.py
@@ -0,0 +1,13 @@
+from ..utils import (
+    text_normalizer,
+    resample_audio
+)
+class ExampleTrainer:
+    def __init__(self, model_name, **kwargs):
+        pass
+    
+    def train(self, **kwargs):
+        pass 
+    
+    def save_checkpoints(self, path):
+        pass 
